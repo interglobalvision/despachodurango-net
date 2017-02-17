767d8b0640d34970d4d396c552011455f1a935b9
cas <cas@interglobal.vision>
2017-02-17 16:46:47 -0600

    add Work post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -3,49 +3,49 @@
 // https://developer.wordpress.org/resource/dashicons/
 function add_menu_icons_styles(){
 ?>
- 
+
 <style>
 #menu-posts-project .dashicons-admin-post:before {
     content: '\f319';
 }
 </style>
- 
+
 <?php
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
 
 
 //Register Custom Post Types
-add_action( 'init', 'register_cpt_project' );
+add_action( 'init', 'register_cpt_work' );
 
-function register_cpt_project() {
+function register_cpt_work() {
 
-    $labels = array( 
-        'name' => _x( 'Projects', 'project' ),
-        'singular_name' => _x( 'Project', 'project' ),
-        'add_new' => _x( 'Add New', 'project' ),
-        'add_new_item' => _x( 'Add New Project', 'project' ),
-        'edit_item' => _x( 'Edit Project', 'project' ),
-        'new_item' => _x( 'New Project', 'project' ),
-        'view_item' => _x( 'View Project', 'project' ),
-        'search_items' => _x( 'Search Projects', 'project' ),
-        'not_found' => _x( 'No projects found', 'project' ),
-        'not_found_in_trash' => _x( 'No projects found in Trash', 'project' ),
-        'parent_item_colon' => _x( 'Parent Project:', 'project' ),
-        'menu_name' => _x( 'Projects', 'project' ),
+    $labels = array(
+        'name' => _x( 'Works', 'work' ),
+        'singular_name' => _x( 'Work', 'work' ),
+        'add_new' => _x( 'Add New', 'work' ),
+        'add_new_item' => _x( 'Add New Work', 'work' ),
+        'edit_item' => _x( 'Edit Work', 'work' ),
+        'new_item' => _x( 'New Work', 'work' ),
+        'view_item' => _x( 'View Work', 'work' ),
+        'search_items' => _x( 'Search Works', 'work' ),
+        'not_found' => _x( 'No works found', 'work' ),
+        'not_found_in_trash' => _x( 'No works found in Trash', 'work' ),
+        'parent_item_colon' => _x( 'Parent Work:', 'work' ),
+        'menu_name' => _x( 'Works', 'work' ),
     );
 
-    $args = array( 
+    $args = array(
         'labels' => $labels,
         'hierarchical' => false,
-        
+
         'supports' => array( 'title', 'editor', 'thumbnail' ),
-        
+
         'public' => true,
         'show_ui' => true,
         'show_in_menu' => true,
         'menu_position' => 5,
-        
+
         'show_in_nav_menus' => true,
         'publicly_queryable' => true,
         'exclude_from_search' => false,
@@ -56,5 +56,5 @@ function register_cpt_project() {
         'capability_type' => 'post'
     );
 
-    register_post_type( 'project', $args );
+    register_post_type( 'work', $args );
 }
