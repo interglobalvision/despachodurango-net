8b06dc3cf5be0154adccac480c6d59c31892e09e
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-02-17 16:50:40 -0600

    scaffold footer

--- footer.php
+++ footer.php
@@ -1,4 +1,21 @@
-  <footer id="footer">
+<?php
+  $logo = IGV_get_option('_igv_site_options', '_igv_site_logo');
+?>
+  <footer id="footer" class="container">
+    <div class="grid-row">
+<?php
+  if (!empty($logo)) {
+?>
+      <div class="grid-item item-s-6">
+        <img src="<?php echo esc_url($logo); ?>" id="site-logo">
+      </div>
+<?php
+  }
+?>
+      <div class="grid-item item-s-6">
+        COPYRIGHT <?php echo date('Y'); ?>
+      </div>
+    </div>
   </footer>
 
 </section>
