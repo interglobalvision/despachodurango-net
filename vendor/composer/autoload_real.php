d0d9ff3da4328ad6cb6982b8e401806b4067af81
cas <cas@interglobal.vision>
2017-02-20 20:22:39 -0600

    add interglobalvision/cmb-field-gallery and fix composer issues

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitc6e5c918bcf4885de9fdb14f7f596262
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitc6e5c918bcf4885de9fdb14f7f596262', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
