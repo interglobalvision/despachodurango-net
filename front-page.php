55ac53ad916f338a1e65df4d9e15fec5acdadb95
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-28 17:10:17 -0500

    add random margin and transition values. update markup for layout. add skrollr keyframes.

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@ get_header();
 
 <main id="main-content">
   <section id="posts">
-    <div class="container">
+    <div id="skrollr-body" class="container">
 
 <?php
 if( have_posts() ) {
@@ -15,7 +15,7 @@ if( have_posts() ) {
 ?>
 
         <article <?php post_class('grid-row justify-center'); ?> id="post-<?php the_ID(); ?>">
-          <div class="grid-item item-s-6 text-align-center font-serif font-size-large">
+          <div class="grid-item item-s-12 item-m-6 text-align-center font-serif font-size-large">
             <?php the_content(); ?>
           </div>
         </article>
@@ -24,37 +24,50 @@ if( have_posts() ) {
     if (!empty($works)) {
 ?>
 
-        <div id="home-work-holder" class="grid-row">
+        <div id="home-work-holder" class="grid-row padding-top-mid padding-bottom-large js-masonry margin-bottom-large">
 
 <?php
       foreach ($works as $work) {
         if (!empty($work['work_id'])) {
 
-          $work_image = get_the_post_thumbnail($work['work_id'], 'home-work', array('class'=>'home-work-image'));
+          $work_image = get_the_post_thumbnail($work['work_id'], 'full', false, array('data-no-lazysizes'=>true, 'class'=>'home-work-image'));
 
           if (!empty($work['image_id'])) {
-            $work_image = wp_get_attachment_image($work['image_id'], 'home-work', array('class'=>'home-work-image'));
+            $work_image = wp_get_attachment_image($work['image_id'], 'full', false, array('data-no-lazysizes'=>true, 'class'=>'home-work-image'));
           }
 
           $work_url = get_the_permalink($work['work_id']);
           $work_title = get_the_title($work['work_id']);
           $work_cats = get_the_category($work['work_id']);
 
+          $trans_y = (rand(-10,10) / 10) * 50;
+          $trans_x = (rand(-10,10) / 10) * 50;
+
+          $margin_top = rand(0,3) * 50;
+          $margin_left = rand(-2,2) * 50;
+          $margin = $margin_top . 'px 0 0 ' . $margin_left . 'px';
 ?>
 
-          <a class="home-work-item" href="<?php echo $work_url; ?>">
-            <?php echo $work_image; ?>
-            <div class="home-work-details grid-column text-align-center justify-around align-items-center">
-              <h2><?php echo $work_title; ?></h2>
-              <?php
-                if (!empty($work_cats)) {
-                  echo '<ul>';
-                  foreach ($work_cats as $cat) {
-                    echo '<li>' . $cat->name . '</li>';
-                  }
-                  echo '</ul>';
-                }
-              ?>
+          <a class="home-work-item grid-item item-s-12 item-m-6 item-l-4" href="<?php echo $work_url; ?>"
+            data-0-bottom-top="transform[sqrt]: translateY(<?php echo -($trans_y); ?>%) translateX(<?php echo -($trans_x); ?>%) scale(.7);"
+            data-0-top-bottom="transform[sqrt]: translateY(<?php echo $trans_y; ?>%) translateX(<?php echo $trans_x; ?>%) scale(1);">
+            <div class="home-work-contents-holder" style="margin: <?php echo $margin; ?>">
+              <div class="home-work-contents text-align-center">
+
+                <div class="home-work-details font-size-basic grid-column text-align-center justify-center align-items-center">
+                  <h2 class="font-size-heading-small font-family-heading font-uppercase"><?php echo $work_title; ?></h2>
+                  <?php
+                    if (!empty($work_cats)) {
+                      echo '<ul>';
+                      foreach ($work_cats as $cat) {
+                        echo '<li>' . $cat->name . '</li>';
+                      }
+                      echo '</ul>';
+                    }
+                  ?>
+                </div>
+                <?php echo $work_image; ?>
+              </div>
             </div>
           </a>
 
@@ -64,6 +77,8 @@ if( have_posts() ) {
 ?>
 
         </div>
+
+        <div id="saturation-filter"></div>
 
 <?php
     }
