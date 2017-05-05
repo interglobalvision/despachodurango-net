32d3e7c39e91e95df122e952a6cf498abeb6a639
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-05 15:06:18 -0500

    add logo to footer as default. adjust markup. add margins

--- partials/footer.php
+++ partials/footer.php
@@ -2,18 +2,20 @@
 <?php
   $logo = IGV_get_option('_igv_site_options', '_igv_site_logo');
 ?>
-  <footer id="footer" class="container">
-    <div class="grid-row">
+  <footer id="footer" class="container margin-top-small margin-bottom-small">
+    <div class="grid-row justify-between align-items-center">
+      <div class="grid-item">
 <?php
   if (!empty($logo)) {
 ?>
-      <div class="grid-item item-s-6">
         <img src="<?php echo esc_url($logo); ?>" id="site-logo">
-      </div>
 <?php
+  } else {
+    get_template_part('partials/durango-logo');
   }
 ?>
-      <div class="grid-item item-s-6 font-family-heading font-lighter font-size-larger">
+      </div>
+      <div class="grid-item font-family-heading font-lighter font-size-larger">
         COPYRIGHT <?php echo date('Y'); ?>
       </div>
     </div>
