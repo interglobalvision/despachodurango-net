1bb6d641227bed65279c2dbcaef57ed874a42d19
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-05-05 15:17:52 -0500

    use footer logo without siteoption override

--- partials/footer.php
+++ partials/footer.php
@@ -1,22 +1,10 @@
-
-<?php
-  $logo = IGV_get_option('_igv_site_options', '_igv_site_logo');
-?>
-  <footer id="footer" class="container margin-top-basic margin-bottom-basic">
-    <div class="grid-row justify-between align-items-center">
-      <div class="grid-item">
-<?php
-  if (!empty($logo)) {
-?>
-        <img src="<?php echo esc_url($logo); ?>" id="site-logo">
-<?php
-  } else {
-    get_template_part('partials/durango-logo');
-  }
-?>
-      </div>
-      <div class="grid-item font-family-heading font-lighter font-size-larger">
-        COPYRIGHT <?php echo date('Y'); ?>
-      </div>
+<footer id="footer" class="container margin-top-basic margin-bottom-basic">
+  <div class="grid-row justify-between align-items-center">
+    <div class="grid-item">
+      <?php get_template_part('partials/durango-logo'); ?>
     </div>
-  </footer>
+    <div class="grid-item font-family-heading font-lighter font-size-larger">
+      COPYRIGHT <?php echo date('Y'); ?>
+    </div>
+  </div>
+</footer>
